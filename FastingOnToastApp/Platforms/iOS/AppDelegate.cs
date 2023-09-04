ef065418c1a9ee5560efb13da1ddaadb80915918
Helen Abell <helen@chaosontoast.co.uk>
2023-09-04 11:55:13 +0100

    Initial App Template

---
+++ FastingOnToastApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FastingOnToastApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
